96225344d9c0c388ae366eb0d568f67bd6e66867
Isaac-Lima <isaac.lima25@aluno.ce.gov.br>
2025-05-21 08:17:33 -0300

    :tada: feat: commit inicial

---
+++ Person/Models/PersonModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+
+namespace Person.Models
+{
+    public class PersonModel
+    {
+        public PersonModel(string name)
+        {
+            Name = name;
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; init; } 
+        public string Name { get; private set; }
+
+        public void ChangeName(string name) { 
+            Name = name;
+        }
+
+        public void setInactive()
+        {
+            Name = "Desativado";
+        }
+    }
+}
